72f1aa1fc89051aff7a1b7fedbcc60a7cfc376e9
José Alberto Estrella <jstar2023@hotmail.com>
2024-05-10 12:57:57 -0600

    EndPoint to Consult Vehicle Paginate

---
+++ backend/src/database/getVehicles.ts
@@ -0,0 +1,28 @@
+import database from "./connection";
+import type { Vehicle } from '../interfaces/vehicle';
+const getVehiclesByQuery = async (criteria: any): Promise<Vehicle[]> => {
+    try {
+        const db = database.getDB()
+        if (!db) {
+            throw new Error('DB is not connected')
+        }
+        const collection = db.collection("vehicles")
+        const results = await collection.find(criteria).toArray()
+        const vehicles: Vehicle[] = results.map(result => ({
+            placa: result.placa,
+            numero_economico: result.numero_economico,
+            vim: result.vim,
+            asientos: result.asientos,
+            seguro: result.seguro,
+            seguro_numero: result.seguro_numero,
+            BRAND: result.BRAND,
+            MODEL: result.MODEL,
+            YEAR: result.YEAR,
+            COLOR: result.COLOR
+        }))
+        return vehicles
+    } catch (error) {
+        throw error
+    }
+}
+export default getVehiclesByQuery
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -4,9 +4,13 @@ import handlerCreateVehicle from '../handlers/createVehicle'
 import handlerDeleteVehicle from '../handlers/deleteVehicle'
 import handlerUpdateVehicle from '../handlers/updateVehicle'
 import handlerFindVehicle from '../handlers/findVehicle'
+import handlerGetVehicles from '../handlers/getVehicles'
 const routes=express.Router()
 
-routes.get('/:id',handlerFindVehicle)
+routes.get('/find/:id',handlerFindVehicle)
+
+routes.get('/search',handlerGetVehicles)
+
 
 routes.post('/create',validateVehicleData,handlerCreateVehicle)
 
